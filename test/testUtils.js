cd5977d9ffcbdca9149961c567ecacea048b7ff2
paulsheehan <p.sheehan94@gmail.com>
2019-03-29 11:17:21 +0000

    Setup store factory helper

--- test/testUtils.js
+++ test/testUtils.js
@@ -1,4 +1,18 @@
 import checkPropTypes from 'check-prop-types';
+import { createStore } from 'redux';
+
+import rootReducer from '../src/reducers';
+
+/**
+ * Create a testing store with imported reducers, middlewate, and an initial state.
+ * globals: rootReducer
+ * @param { object } initialState - Initial state for store
+ * @function storeFactory
+ * @returns {Store} - Redux store. 
+ */
+export const storeFactory = (initialState) => {
+    createStore(rootReducer, initialState);
+}
 
 /**
  * Return node(s) with the given data-test attribute
@@ -19,3 +33,4 @@ export const checkProps = (component, conformingProps) => {
     );
     expect(propError).toBeUndefined();
 }
+
